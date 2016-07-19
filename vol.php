d8583b07bd8befc12128b5e87c68581589330672
Ewoud Dronkert <ednl@users.noreply.github.com>
2016-07-19 16:25:48 +0200

    Simpler version

--- vol.php
+++ vol.php
@@ -7,17 +7,11 @@ if (isset($_GET['adj'])) {
 	$adj = preg_replace('/[^0-9+-]+/', '', $_GET['adj']);
 	if (strlen($adj))
 		if (!strcmp($adj, '-') || !strcmp($adj, '+') || is_numeric($adj))
-			// Adjust volume
+			// Adjust volume, discard output
 			exec($radio . ' vol ' . $adj);
 }
 
-// Get status
-unset($output);
-exec($radio, $output);
-foreach ($output as $line) {
-	$trimmed = trim($line);
-	if (strlen($trimmed))
-		echo $trimmed . PHP_EOL;
-}
+// Get status, display output
+system($radio);
 
 ?>
