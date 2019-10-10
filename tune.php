6a43fbbf06ca37cd4a0fc38e7b0dec521a111fe6
Ewoud Dronkert <ednl@users.noreply.github.com>
2019-10-10 20:48:36 +0200

    Station IDs can be longer now

--- tune.php
+++ tune.php
@@ -3,8 +3,8 @@
 $radio = '/usr/local/bin/piradio';
 
 if (isset($_GET['station'])) {
-	// Station IDs may consist of digits or lowercase letters, max 7 characters
-	$station = substr(preg_replace('/[^a-z0-9]+/', '', $_GET['station']), 0, 7);
+	// Station IDs may consist of digits or lowercase letters, max 15 characters
+	$station = substr(preg_replace('/[^a-z0-9]+/', '', $_GET['station']), 0, 15);
 	if (strlen($station))
 		// Tune to station, discard output
 		exec($radio . ' ' . $station);
